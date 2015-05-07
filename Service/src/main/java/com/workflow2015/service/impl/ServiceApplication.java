6060c1a413756770c77a389501dc397e168ad6fe
Dominik Heigl <heigdomi@gmail.com>
2015-05-07 16:16:03 +0200

    first http get working with camel (camel context)

--- Service/src/main/java/com/workflow2015/service/impl/ServiceApplication.java
+++ Service/src/main/java/com/workflow2015/service/impl/ServiceApplication.java
@@ -1,24 +1,50 @@
 package com.workflow2015.service.impl;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.apache.camel.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Dominik Heigl on 5/6/15.
  */
-@Service
+@org.springframework.stereotype.Component
 public class ServiceApplication {
 
-    public ServiceApplication() {
+    private final CamelContext camelContext;
+
+    @Autowired
+    public ServiceApplication(CamelContext camelContext) {
+        this.camelContext = camelContext;
     }
 
+
     public void test() {
-        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ProducerTemplate template = this.camelContext.createProducerTemplate();
+            this.camelContext.start();
+
+            Exchange exchange = template.request("http://www.google.com/search", new Processor() {
+                public void process(Exchange exchange) throws Exception {
+                }
+            });
+
+            if (null != exchange) {
+                Message out = exchange.getOut();
+                int responseCode = out.getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class);
+                System.out.println("Response: " + String.valueOf(responseCode));
+            }
+
+            Thread.sleep(1000 * 3);
+            this.camelContext.stop();
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+
+        System.out.println("DONE!!");
+        /*RestTemplate restTemplate = new RestTemplate();
         Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
         System.out.println("Name:    " + page.getName());
         System.out.println("About:   " + page.getAbout());
         System.out.println("Phone:   " + page.getPhone());
-        System.out.println("Website: " + page.getWebsite());
+        System.out.println("Website: " + page.getWebsite());*/
     }
 }
